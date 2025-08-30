20911198242eadd4bef408a9d0e96629de33d624
shehriyar ali <shehriyar.ali.work@gmail.com>
2025-08-30 22:45:38 +0500

    fixed the laravel framework issues

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,21 +7,23 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&display=swap"  rel="stylesheet">
-    
-    @vite('resources/css/home.css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
+
+    @vite(['resources/css/home.css'])
 </head>
 <body>
     
     <div style="height: 100vh; width: 100vw; display: flex; flex-direction: column; align-items: center; justify-content: center;">
-        <div class="logo">
-            Tranquil<span class="dot">.</span>
-        </div>
 
-    <div class="btns" style="margin-top: 20px;">
-        <a href="./new-note.blade.php" class="btn">New Note</a>
-        <a href="./howtouse.blade.php" class="btn">How to Use</a>
-        <a href="./saved-notes.blade.php" class="btn">Saved Notes</a>
-        <a href="./settings.blade.php" class="btn">Settings</a>
+    <div class="logo">
+        Tranquil<span class="dot">.</span>
+    </div>
+
+    <div class="btns-section">
+        <a href="./new-note.blade.php" class="btn">New Note <i class="fa-solid fa-plus"></i></a>
+        <a href="./howtouse.blade.php" class="btn">How to Use <i class="fa-solid fa-question"></i></a>
+        <a href="./saved-notes.blade.php" class="btn">Saved Notes <i class="fa-solid fa-bookmark"></i></a>
+        <a href="./settings.blade.php" class="btn">Settings <i class="fa-solid fa-gear"></i></a>
     </div>
 
 </body>
